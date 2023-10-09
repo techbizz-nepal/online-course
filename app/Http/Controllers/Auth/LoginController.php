8659c0b6ff7f896ae5d1dcfaa0eaed0edbcfde8e
techbizz-nepal <techbizznepal@gmail.com>
2023-10-10 02:49:29 +0545

    add: logout logic and other clean up

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +31,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/dashboard';
+    protected string $redirectTo = '/admin/dashboard';
 
     /**
      * Create a new controller instance.
@@ -36,5 +41,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout(Request $request): RedirectResponse|Application|Redirector|JsonResponse|\Illuminate\Foundation\Application
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
     }
 }
--- app/Http/Controllers/Auth/Student/LoginController.php
+++ app/Http/Controllers/Auth/Student/LoginController.php
@@ -7,8 +7,10 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -23,10 +25,6 @@ class LoginController extends Controller
         $this->middleware('guest:student')->except('logout');
     }
 
-    protected function redirectTo(Request $request): string
-    {
-        return route('student.getLoginForm');
-    }
     public function showLoginForm(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
         return view('auth.student.login');
@@ -35,14 +33,29 @@ class LoginController extends Controller
     public function login(Request $request): RedirectResponse
     {
         $this->validate($request, [
-            'email'   => 'required|email',
+            'email' => 'required|email',
             'password' => 'required|min:6'
         ]);
-        if (Auth::guard('student')->attempt($request->only(['email','password']), $request->get('remember'))){
+        if (Auth::guard('student')->attempt($request->only(['email', 'password']), $request->get('remember'))) {
             return redirect()->intended($this->redirectTo);
         }
 
         return back()->withInput($request->only('email', 'remember'));
+    }
+
+    public function logout(Request $request): RedirectResponse|\Illuminate\Contracts\Foundation\Application|Redirector|JsonResponse|\Illuminate\Foundation\Application
+    {
+        Auth::guard('student')->logout();
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
     }
 
 }
