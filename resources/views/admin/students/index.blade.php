8fdc01da5ecc55b1403469b1cb79a22e704d2903
techbizz-nepal <techbizznepal@gmail.com>
2023-10-10 02:52:52 +0545

    optimize: student table in admin dashboard

--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -12,50 +12,62 @@
                 <tr>
                     <th class="text-center">#</th>
                     <th class="text-center" style="width: 30%;">Student</th>
+                    <th class="text-center">Email</th>
                     <th class="text-center">PDF File</th>
-                    <th class="text-center">QR Code</th>
                     <th class="text-center">Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                @if(count($students) > 0)
-                @foreach($students as $student)
-                <tr>
-                    <th scope="row">{{ $loop->iteration }}</th>
-                    <td class="text-center">{{ $student->name }}</td>
-                    <td class="text-center">
-                        <a
-                            href="{{ asset($student->pdf) }}"
-                            class="btn btn-blueLight"
-                            target="_blank"
-                        >
-                            View File
-                        </a>
-                    </td>
-                    <td class="text-center">
-                        {!! QrCode::format('svg')->size(100)->generate(asset($student->pdf)); !!}
-                    </td>
-                    <td class="text-center">
-                        <a href="{{ route('admin.student.qr', $student) }}" class="btn btn-secondary mb-1">Download QR</a>
-                        <br>
-                        <a href="{{ route('admin.student.edit', $student) }}" class="btn btn-info mb-1">Edit</a>
-                        <br>
-                        <a href="javascript:void(0)" onclick="document.getElementById('deleteStudent{{ $loop->iteration }}').submit();" class="btn btn-danger">Delete</a>
-                        <form action="{{ route('admin.student.destroy', $student) }}" class="d-none" method="POST" id="deleteStudent{{ $loop->iteration }}">
-                            @csrf
-                            @method('DELETE')
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
+                @if(count($students->items()) > 0)
+                    @foreach($students->items() as $student)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $student->name }}</td>
+                            <td>{{$student->email}}</td>
+                            <td>
+                                <a
+                                    href="{{ asset($student->pdf) }}"
+                                    class="btn btn-blueLight"
+                                    target="_blank"
+                                >
+                                    View File
+                                </a>
+                            </td>
+                            {{--                    <td class="text-center">--}}
+                            {{--                        {!! QrCode::format('svg')->size(100)->generate(asset($student->pdf)); !!}--}}
+                            {{--                    </td>--}}
+                            <td class="text-center">
+                                <a href="{{ route('admin.student.qr', $student) }}" class="btn btn-secondary mb-1">Download
+                                    QR</a>
+                                <a href="{{ route('admin.student.edit', $student) }}" class="btn btn-info mb-1">Edit</a>
+                                <a href="javascript:void(0)"
+                                   onclick="document.getElementById('deleteStudent{{ $loop->iteration }}').submit();"
+                                   class="btn btn-danger">Delete</a>
+                                <form action="{{ route('admin.student.destroy', $student) }}" class="d-none"
+                                      method="POST" id="deleteStudent{{ $loop->iteration }}">
+                                    @csrf
+                                    @method('DELETE')
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
                 @else
-                <tr>
-                    <td class="text-center" colspan="5">No Data Found</td>
-                </tr>
+                    <tr>
+                        <td class="text-center" colspan="5">No Data Found</td>
+                    </tr>
                 @endif
 
                 </tbody>
             </table>
+            <div class="py-4">
+                <a type="button"
+                   href="{{$prev_page_url ?? "#"}}"
+                   class="{{$prev_page_url ?? "disabled"}} btn btn-primary btn-sm">Previous</a>
+                <a type="button"
+                   href="{{$next_page_url ?? "#"}}"
+                   class="{{$next_page_url ?? "disabled"}} btn btn-primary btn-sm">Next</a>
+            </div>
         </div>
+
     </div>
 @endsection
