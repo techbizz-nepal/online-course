e412b19682a588486ef33e336b9a55b163909e75
techbizz-nepal <techbizznepal@gmail.com>
2023-10-10 02:49:29 +0545

    add: logout logic and other clean up

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,2 +1,8 @@
 <h1>Welcome Student</h1>
+<a href="javascript:void(0);" onclick="document.getElementById('logoutForm').submit();">
+    <i class="fa fa-close"></i>Logout
+    <form action="{{ route('logout') }}" method="POST" class="d-none" id="logoutForm">
+        @csrf
+    </form>
+</a>
 {{ auth()->guard('student')->user() }}
