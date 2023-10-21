9f28c87ceebebc235f5ed2cea4749d9a76162c21
techbizz-nepal <techbizznepal@gmail.com>
2023-10-22 03:30:57 +0545

    refactor

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,8 +1,12 @@
-<h1>Welcome Student</h1>
-<a href="javascript:void(0);" onclick="document.getElementById('logoutForm').submit();">
-    <i class="fa fa-close"></i>Logout
-    <form action="{{ route('student.postLogout') }}" method="POST" class="d-none" id="logoutForm">
-        @csrf
-    </form>
-</a>
-{{ auth()->guard('student')->user() }}
+
+<h1>Under Construction</h1>
+{{--{{ auth()->guard('student')->user()->name }}--}}
+<div class="row flex-row">
+    <h2>Welcome Student</h2>
+    <a href="javascript:void(0);" onclick="document.getElementById('logoutForm').submit();">
+        <i class="fa fa-close"></i>Logout
+        <form action="{{ route('student.postLogout') }}" method="POST" class="d-none" id="logoutForm">
+            @csrf
+        </form>
+    </a>
+</div>
