843651220fbdb23e789eb286eff95bd889702c10
susant <techbizznepal@gmail.com>
2023-12-17 23:13:23 +0545

    wip: student view detail

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group {{$cols}}">
     <label for="{{$id}}">{{$label}}</label>
     <input @required($required)
-           class="form-control"
+           class="form-control mb-2"
            id="{{$id}}"
            type="{{$type}}"
            name="{{$name}}"
@@ -10,4 +10,13 @@
            @readonly($readonly)
            @if($pattern) pattern="{{$pattern}}" @endif
     />
+    @if($helpText !== '')
+        <span>
+            @if($helpLink !== "#")
+                <a target="_blank" href="{{$helpLink}}">{{$helpText}}</a>
+            @else
+                {{$helpText}}
+            @endif
+        </span>
+    @endif
 </div>
