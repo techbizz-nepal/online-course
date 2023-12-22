8a490eabc169ef74ca918e146478efeaafda7f03
susant <susanta.paudel@tala.co>
2023-12-23 04:58:03 +0545

    student survey crud

--- resources/views/admin/students/show.blade.php
+++ resources/views/admin/students/show.blade.php
@@ -15,27 +15,33 @@
                 @endforeach
             </div>
             <div class="row mb-3">
-                <div class="col-12" id="heading">
-                    <h4 class="mb-4 bg-flat-color-1 p-3 text-white">
-                        Survey
-                    </h4>
-                </div>
-
                 @foreach($survey as $key => $category)
-                                    @dump($category)
                     <div class="col-3 mb-2">
-                        <p class="font-weight-bold mb-2">{{str($key)->replace('_', ' ')->title()}}</p>
+                        <p class="font-weight-bold mb-2 bg-flat-color-4 pl-2 text-white">{{str($key)->replace('_', ' ')->title()}}</p>
                         @if(!is_array($category))
-                            <span
-                                class="font-weight-normal ">{{$category ? str($category)->title() : 'N/A'}}</span>
+                            <span class="font-weight-normal ">{{$category ? str($category)->title() : 'N/A'}}</span>
                         @else
                             @foreach($category as $categoryKey => $categoryQuestion)
-                                <div class="row col-12 mb-1">
-                                    <p>{{$categoryQuestion['label']?? null}} </p>
-                                </div>
-                                <div class="row col-12 mb-3 font-italic pl-5">
-                                    <p>{{$categoryQuestion['value'] ?? null}}</p>
-                                </div>
+                                @if($categoryKey == "education_achievement")
+                                    <p class="mb-3">{{$categoryQuestion['label']}}</p>
+                                    @foreach($categoryQuestion['level'] as $key => $value)
+                                        <p>{{str($key)->replace('_', ' ')->title()->replace('i', 'I')}}: {{str($value['value'])->title()}}</p>
+                                    @endforeach
+                                @endif
+                                @if($key == 'disabilities' && $categoryQuestion != null)
+                                    <p class="font-italic pl-3">{{str($categoryQuestion)?->replace('-', ' ')->start('- ')}}</p>
+                                @endif
+                                @if(isset($categoryQuestion['value']))
+                                    @if($categoryQuestion['value'] == 'other')
+                                        @continue(true)
+                                    @endif
+                                    <div class="row col-12 mb-1">
+                                        <p>{{$categoryQuestion['label']?? null}} </p>
+                                    </div>
+                                    <div class="row col-12 mb-3 font-italic pl-3">
+                                        <p>{{str($categoryQuestion['value'])?->replace('-', ' ')->start('- ')}}</p>
+                                    </div>
+                                @endif
                             @endforeach
                         @endif
 
